8ecff765ce35aae80c3e689a88a10bdffa8ce6d7
Raiyan A. Malik <87821694+Th3BlackHol3@users.noreply.github.com>
2026-09-04 11:16:51 -0400

    Main File/Tool

---
+++ RAMCleaner.sh
@@ -0,0 +1,1 @@
+sudo sync && sudo sync && echo 1 | sudo tee /proc/sys/vm/drop_caches && sudo sync && echo 2 | sudo tee /proc/sys/vm/drop_caches && sudo sync && echo 3 | sudo tee /proc/sys/vm/drop_caches && swapoff -a && swapon -a && printf 'Ram-cache and Swap Cleared!' && free -h
